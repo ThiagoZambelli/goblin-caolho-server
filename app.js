c927c8695ed55ddc3662f718324956f732562bc6
ThiagoZambelli <thiagozamoliveira@gmail.com>
2023-07-01 17:09:42 -0300

    fix: coreção de permição de requisição

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import rotaItens from './rotas/itens.js';
 import rotaUsers from './rotas/user.js';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
+app.use(cors({origin: "*"}))
 app.use('/itens', rotaItens);
 app.use('/users', rotaUsers);
 
